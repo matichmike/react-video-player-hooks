b5d9416740e884dd0255b7b0143d11878f729b29
Michael Matich <matich.mike@gmail.com>
2020-09-01 11:34:54 -0400

    refactor App to hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,6 @@ import React, {useState} from 'react';
 const SearchBar = ({onFormSubmit}) => {
   const [term, setTerm] = useState('');
 
-  const onInputChange = (event) => {
-    setTerm(event.target.value);
-  };
-
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -21,7 +17,7 @@ const SearchBar = ({onFormSubmit}) => {
           <input 
           type="text" 
           value={term}
-          onChange={onInputChange}
+          onChange={(event) => setTerm(event.target.value)}
           />
         </div>
       </form>
